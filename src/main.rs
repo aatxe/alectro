0aaae03aeb93fcbe4cf82d79cf29cc8ed97437be
Aaron Weiss <awe@pdgn.co>
2017-07-19 16:20:56 -0400

    Converted listener into an async style.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
+extern crate futures;
 extern crate irc;
 extern crate termion;
+extern crate tokio_core;
 
 use std::io::prelude::*;
 use std::io;
-use std::sync::{Arc, mpsc};
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 
+use futures::sync::mpsc;
 use irc::client::prelude::*;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use tokio_core::reactor::Core;
 
 fn main() {
-    let terminate = Arc::new(AtomicBool::new(false));
-    let terminate_stdin = terminate.clone();
+    let mut reactor = Core::new().unwrap();
 
     let cfg = Config {
         nickname: Some(format!("aatxe")),
@@ -27,17 +28,12 @@ fn main() {
     let irc_server = IrcServer::from_config(cfg).unwrap();
     irc_server.identify().unwrap();
 
-    let (keys_tx, keys_rx) = mpsc::channel();
+    let (keys_tx, keys_rx) = mpsc::unbounded();
     let _ = thread::spawn(move || {
-        let terminate = terminate_stdin;
         let stdin = io::stdin();
         let mut input = stdin.keys();
 
-        while let Some(res_key) = input.next() {
-            if terminate.load(Ordering::Relaxed) {
-                break
-            }
-
+        for res_key in input.next() {
             if let Ok(key) = res_key {
                 keys_tx.send(key).unwrap();
             }
@@ -51,15 +47,23 @@ fn main() {
            termion::cursor::Goto(1, 1),
            termion::cursor::Hide).unwrap();
 
-    let mut stream_iter = irc_server.stream().wait();
-    while let Some(res) = stream_iter.next() {
-        write!(stdout, "{}", res.unwrap()).unwrap();
+    let output_future = irc_server.stream().for_each(|message| {
+        print!("{}", message);
+        Ok(())
+    });
 
-        if let Ok(Key::Char('q')) = keys_rx.try_recv() {
-            irc_server.send_quit("User sent a quit command.").unwrap();
-            break;
+    let input_future = keys_rx.map_err(|()| unreachable!()).for_each(|key| {
+        match key {
+            Key::Char('q') => {
+                irc_server.send_quit("QUIT")?;
+            }
+            _ => (),
         }
-    }
+
+        Ok(())
+    });
+
+    reactor.run(output_future.join(input_future)).unwrap();
 
     write!(stdout, "{}", termion::cursor::Show).unwrap();
     stdout.flush().unwrap();
