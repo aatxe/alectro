d0708ada5574ccea7fea572987f7c0378db31077
Aaron Weiss <awe@pdgn.co>
2017-07-28 11:51:43 -0400

    Improved error_chain setup.

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,16 @@
 error_chain! {
+    links {
+        Irc(::irc::error::Error, ::irc::error::ErrorKind);
+    }
+
     foreign_links {
-        Irc(::irc::error::Error);
+        SendKey(::futures::sync::mpsc::SendError<::termion::event::Event>);
+    }
+
+    errors {
+        ThreadJoinErr(e: String) {
+            description("Attempted to join on panicked thread.")
+            display("Attempted to join on panicked thread. Thread panicked with:\n{}", e)
+        }
     }
 }
